3d4f0548a3ae7e3044c8e7595eef76e4d66be8ac
Илья Зубанов <zubanov.bz@mail.ru>
2022-06-03 19:51:50 +0300

    ныа

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,13 @@
+from django.shortcuts import render
 
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):    
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request,template)
 
-def group_posts(request,slug):    
-    return HttpResponse('Страница с постами')
+def group_posts(request,slug):  
+    template = 'posts/group_list.html'  
+    return render(request,template)
